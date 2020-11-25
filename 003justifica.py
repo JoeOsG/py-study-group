3dca1856d1472ab79d13fdba3c799151077e66a6
JoeOsG <josedoomaor+github@gmail.com>
2020-11-25 12:49:35 -0800

    Update 003justifica.py

--- 003justifica.py
+++ 003justifica.py
@@ -1,14 +1,11 @@
 def justifica(trabajo):
-#  treinta = []
-#  treinta[:0] = trabajo
   if len(trabajo) < 29:
     contador = len(trabajo)
     
     while contador < 30:
-#      print('e')
       trabajo = trabajo + ' '
       contador+=1
-#    
+    
     trabajo = trabajo + '\n'
 
   print(trabajo[0:30])
